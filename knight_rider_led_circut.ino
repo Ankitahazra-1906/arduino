fc831552c292256e8cc7157134a03c0b7de33af6
Tanuka <54015607+Tanuka-Mondal@users.noreply.github.com>
2020-10-27 18:34:46 +0530

    Update knight_rider_led_circut.ino

--- knight_rider_led_circut.ino
+++ knight_rider_led_circut.ino
@@ -9,9 +9,9 @@ void loop()
   int reading = analogRead(A0);
   float voltage = reading * 5.0;
   voltage /= 1024.0; 
-  float temperatureC = (voltage - 0.5) * 100 ;
-  float temperatureK = temperatureC + 273;
-  if(temperatureK >= 279 )
+  float tempC = (voltage - 0.5) * 100 ;
+  float tempK = tempC + 273;
+  if(tempK >= 279 )
   {
     digitalWrite(5,HIGH);
   }
